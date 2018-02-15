bbcbe2125897cad26b9d3686a56bb465e759ad42
ahelsayed <ahmed.m.elsayed93@gmail.com>
2018-02-15 22:47:42 +0200

    clean code

--- bot.py
+++ bot.py
@@ -1,24 +1,46 @@
-import yaml, time, os, mongoengine
+import yaml, os, mongoengine
 from db import Session, Order
 from telegram.ext import Updater, CommandHandler
 from telegram import ParseMode
 from jinja2 import Environment, FileSystemLoader
+from functools import wraps
 
-def __isDigit(string):
+# tools
+
+def session_check(func):
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        bot, update = args
+        chat_id = str(update.message.chat_id)
+        username = update.message.from_user.username        
+        session = Session.get(chat_id=chat_id)
+        
+        if not session:
+            update.message.reply_text('No active session, please start a new one')
+            return
+
+        kwargs.update(
+            chat_id=chat_id, 
+            username=username, 
+            session=session
+        )
+
+        return func(*args, **kwargs)
+    return decorator
+
+def is_digit(string):
     try:
         float(string)
         return True
     except ValueError:
         return False
 
-def __get_order_details(order_string):
+def extract_order_details(order_string):
         order = ''
         quantity = None
         for word in order_string.split(' '):
-            #quantity
             if word.isdigit() and not quantity:
                 quantity = int(word)
-            # order
             else:
                 if order:
                     order += ' '
@@ -28,88 +50,92 @@ def __get_order_details(order_string):
 
         return quantity, order
 
-def showHelp(bot, update):
-    chat_id = str(update.message.chat.id)
-    text = j2_env.get_template('help.html').render()
-    bot.send_message(chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
+def render_template(template, **kwargs):
+    return j2_env.get_template(template).render(**kwargs)
 
-def startSession(bot, update):
+# handlers
+
+def show_help(bot, update):
+    """ Show help message when command /help is issued """
+
+    chat_id = str(update.message.chat.id)
+    msg = render_template('help.html')
+    update.message.reply_text(msg, parse_mode=ParseMode.HTML)
+
+def start_session(bot, update):
+    """ Start new session when command /start is issued """
+
     chat_id = str(update.message.chat.id)
     username = update.message.from_user.username
 
     if Session.get(chat_id=chat_id):
-        text = 'Can\'t start new session, please end the current session'
-        bot.send_message(chat_id=chat_id, text=text)
+        update.message.reply_text('A session is already started')
         return 
     
     session = Session(chat_id=chat_id, created_by=username)
     session.save()
+    
+    update.message.reply_text('New session is started')
 
-    text = 'New session is started'
-    bot.send_message(chat_id=chat_id, text=text)
+@session_check
+def end_session(bot, update, **kwargs):
+    """ End active session when command /end is issued """
 
-def endSession(bot, update):
-    chat_id = str(update.message.chat.id)
-    if Session.get(chat_id=chat_id):
-        Session.get(chat_id=chat_id).delete()
+    session = kwargs.get('session')
+    session.delete()
+    update.message.reply_text('Session is ended')
 
-    text = 'Session is ended'
-    bot.send_message(chat_id=chat_id, text=text)
+@session_check
+def set_price(bot, update, **kwargs):
+    """ Set order price when command /set <order> = <price> is issued """
 
-def setPrice(bot, update):
-    chat_id = str(update.message.chat.id)
-    session = Session.get(chat_id=chat_id)
-    if not session:
-        return
-
+    session = kwargs.get('session')
     orders = update.message.text.replace('/set ', '').split(',')
+
     for order in orders:
         order, price = [x.strip() for x in order.split('=')]
-        if __isDigit(price):
+        if is_digit(price):
             price = abs(float(price))
             Order.objects(session=session, order=order).update(price=price)
 
-def setService(bot, update):
-    chat_id = str(update.message.chat.id)
-    session = Session.get(chat_id=chat_id)
-    if not session:
-        return
+@session_check
+def set_service(bot, update, **kwargs):
+    """ Set service when command /service is issued """
 
+    session = kwargs.get('session')
     service = update.message.text.replace('/service ', '').strip()
-    if __isDigit(service):
+    if is_digit(service):
         service = abs(float(service))
         session.update(service=service)
 
-def setTax(bot, update):
-    chat_id = str(update.message.chat.id)
-    session = Session.get(chat_id=chat_id)
-    if not session:
-        return
-        
+@session_check
+def set_tax(bot, update, **kwargs):
+    """ Set tax when command /tax is issued """
+
+    session = kwargs.get('session')
     tax = update.message.text.replace('/tax ', '').strip()
-    if __isDigit(tax):
+    if is_digit(tax):
         tax = abs(float(tax))
         session.update(tax=tax)
-    
+        
+@session_check
+def my_orders(bot, update, **kwargs):
+    """ List user's orders when command /me is issued """
 
-def myOrders(bot, update):
-    chat_id = str(update.message.chat.id)
-    username = update.message.from_user.username    
-    session = Session.get(chat_id=chat_id)
-
-    if not session:
-        return
+    session = kwargs.get('session')
+    username = kwargs.get('username')
 
     orders = Order.objects.filter(session=session, username=username)
-    text = j2_env.get_template('me.html').render(orders=orders, username=username)
-    update.message.reply_text(text=text, parse_mode=ParseMode.HTML)
+    msg = render_template('me.html', orders=orders, username=username)
+    update.message.reply_text(msg, parse_mode=ParseMode.HTML)
 
-def allOrders(bot, update):
-    chat_id = str(update.message.chat.id)
-    session = Session.get(chat_id=chat_id)
-
-    if not session:
-        return
+@session_check
+def all_orders(bot, update, **kwargs):
+    """ List all orders when command /all is issued """
+    
+    chat_id = kwargs.get('chat_id')
+    session = kwargs.get('session')
+    username = kwargs.get('username')
 
     pipeline = [
         {'$match': {
@@ -127,21 +153,24 @@ def allOrders(bot, update):
     ]
 
     orders = Order.objects.aggregate(*pipeline)
-    text = j2_env.get_template('all.html').render(orders=orders)
-    bot.send_message(chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
+    msg = render_template('all.html', orders=orders)
+    update.message.reply_text(msg, parse_mode=ParseMode.HTML)
 
-def bill(bot, update):
-    chat_id = str(update.message.chat.id)
-    session = Session.get(chat_id=chat_id)
-    normalizedService = 0
-    normalizedTax = 0
+@session_check
+def bill(bot, update, **kwargs):
+    """ Show bill when command /bill is issued """
     
-    if not session:
-        return
+    chat_id = kwargs.get('chat_id')
+    session = kwargs.get('session')
+
+    normalized_service = 0
+    normalized_tax = 0
+    total_service = session.service
+    total_tax = session.tax
 
     pipeline = [
         {
-            '$match': {'session':session.id}
+            '$match': {'session': session.id}
         },
         {
             '$group':{
@@ -154,32 +183,30 @@ def bill(bot, update):
     unknown_orders = Order.objects(price=None)
     bill = Order.objects.aggregate(*pipeline)
 
-    totalService = session.service
-    totalTax = session.tax
-
-    numberOfPersons = len(Order.objects.distinct('username'))
+    number_of_persons = len(Order.objects.distinct('username'))
     
-    if numberOfPersons:
-        normalizedService = totalService / numberOfPersons
-        normalizedTax = totalTax / numberOfPersons
+    if number_of_persons:
+        normalized_service = total_service / number_of_persons
+        normalized_tax = total_tax / number_of_persons
 
-    text = j2_env.get_template('bill.html').render(
+    msg = render_template(
+        'bill.html',
         bill=bill, 
         unknown_orders=unknown_orders,
-        totalService=totalService,
-        totalTax=totalTax,
-        normalizedService=normalizedService,
-        normalizedTax=normalizedTax,
+        total_service=total_service,
+        total_tax=total_tax,
+        normalized_service=normalized_service,
+        normalized_tax=normalized_tax,
     )
-    bot.send_message(chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
+    update.message.reply_text(msg, parse_mode=ParseMode.HTML)
 
-def addOrder(bot, update):
-    chat_id = str(update.message.chat.id)
-    session = Session.get(chat_id=chat_id)
-    username = update.message.from_user.username
-
-    if not session:
-        return
+@session_check
+def add_order(bot, update, **kwargs):
+    """ Add new order(s) when command /add is issued """
+    
+    chat_id = kwargs.get('chat_id')
+    session = kwargs.get('session')
+    username = kwargs.get('username')
 
     if update.message.reply_to_message:
         payload = update.message.reply_to_message.text.replace('/add', '')
@@ -189,36 +216,36 @@ def addOrder(bot, update):
     orders = payload.split('+')
 
     for order_string in orders:
-        quantity, order = __get_order_details(order_string.strip())
+        quantity, order = extract_order_details(order_string.strip())
 
         if not order:
+            update.message.reply_text('Invalid order')
             return
         
-        exists_order = Order.get(
-            session=session, 
-            username=username, 
-            order=order
-        )
-        
+        # check if the order exists in user's order
+        exists_order = Order.get(session=session, 
+                                 username=username, 
+                                 order=order)
+
+        # if the order exists in user's order, increment quantity
         if exists_order:
             exists_order.update(inc__quantity=quantity)
+        
+        # else, record the new order
         else: 
-            order_object = Order(
-                session=session,
-                username=username,
-                quantity=quantity,
-                order=order
-            )
+            order_object = Order(session=session, 
+                                 username=username, 
+                                 quantity=quantity, 
+                                 order=order)
             order_object.save()
 
-def deleteOrder(bot, update):
-    chat_id = str(update.message.chat.id)
-    session = Session.get(chat_id=chat_id)
-    username = update.message.from_user.username
-
-    if not session:
-        return
-
+@session_check
+def delete_order(bot, update, **kwargs):
+    """ Delete order(s) when command /delete is issued """
+    
+    session = kwargs.get('session')
+    username = kwargs.get('username')
+    
     if update.message.reply_to_message:
         payload = update.message.reply_to_message.text.replace('/add', '')
     else:
@@ -227,36 +254,45 @@ def deleteOrder(bot, update):
     orders = payload.split('+')
 
     for order_string in orders:
-        quantity, order = __get_order_details(order_string.strip())
-        
+        quantity, order = extract_order_details(order_string.strip())
         quantity = abs(int(quantity))
 
-        order_obj = Order.get(session=session, username=username, order=order)
+        order_obj = Order.get(session=session, 
+                              username=username, 
+                              order=order)
+                              
         if order_obj:
             if quantity >= order_obj.quantity:
                 order_obj.delete()
             else:
                 order_obj.update(inc__quantity= -quantity)
 
-
-def main():
-    # handlers
+def main():    
+    
     updater = Updater(config['telegram']['token'])
-    updater.dispatcher.add_handler(CommandHandler('start', startSession))
-    updater.dispatcher.add_handler(CommandHandler('end', endSession))
-    updater.dispatcher.add_handler(CommandHandler('add', addOrder))
-    updater.dispatcher.add_handler(CommandHandler('delete', deleteOrder))
-    updater.dispatcher.add_handler(CommandHandler('set', setPrice))
-    updater.dispatcher.add_handler(CommandHandler('service', setService))
-    updater.dispatcher.add_handler(CommandHandler('tax', setTax))
-    updater.dispatcher.add_handler(CommandHandler('me', myOrders))
-    updater.dispatcher.add_handler(CommandHandler('all', allOrders))
-    updater.dispatcher.add_handler(CommandHandler('bill', bill))
-    updater.dispatcher.add_handler(CommandHandler('help', showHelp))
+
+    dp = updater.dispatcher
+
+    # handlers
+
+    dp.add_handler(CommandHandler('start', start_session))
+    dp.add_handler(CommandHandler('end', end_session))
+    dp.add_handler(CommandHandler('add', add_order))
+    dp.add_handler(CommandHandler('delete', delete_order))
+    dp.add_handler(CommandHandler('set', set_price))
+    dp.add_handler(CommandHandler('service', set_service))
+    dp.add_handler(CommandHandler('tax', set_tax))
+    dp.add_handler(CommandHandler('me', my_orders))
+    dp.add_handler(CommandHandler('all', all_orders))
+    dp.add_handler(CommandHandler('bill', bill))
+    dp.add_handler(CommandHandler('help', show_help))
+
     updater.start_polling()
+    
     updater.idle()
 
 if __name__ == '__main__':
+    
      # load configrations 
     with open('config.yaml', 'r') as config_file:
         config = yaml.load(config_file)
